112b7a5bbe5b21e85a4f8026a7af92c33fc387f6
saiprasaad <saiprasaad@gmail.com>
2021-01-07 12:41:44 +0530

    python_programs

---
+++ imagetoasciiart.py
@@ -0,0 +1,6 @@
+import pywhatkit
+
+path_to_image='flower.png'
+output_file='ascii_art.txt'
+
+pywhatkit.pywhatkit.image_to_ascii_art(path_to_image,output_file)
---
+++ text_to_handwriting.py
@@ -0,0 +1,2 @@
+import pywhatkit
+pywhatkit.text_to_handwriting('Human vision is the primary source of information',rgb=[0,0,0])
